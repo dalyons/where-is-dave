6cf22137486d7e1f35e44fd1a8fbddd694aae0c9
Dave Lyons <dalyons@gmail.com>
2013-07-09 15:33:47 +0200

    sweet FB photo data extraction

--- fb_munger.js
+++ fb_munger.js
@@ -1,13 +1,68 @@
 
-function fetch(access_token) {
+function fetch(access_token, onComplete) {
+  var url = buildUrl(access_token);
   
+  $.get(url).success(function(json){
+    var results = extractResults(json);
+    var photos = results.photos;
+    
+    //merge place info into photos
+    mergePlaceInfo(photos, results.places);
+    
+    //handle backdates
+    reconcileBackdates(photos);
+    
+    //sort by timestamp
+    photos = _.sortBy(photos, function(p) { return p.created });
+    
+    //remove all the crap
+    photos = standardize(photos);
+    
+    onComplete(photos);
+  });
 }
+
+function mergePlaceInfo(photos, places) {
+  var coordMap = {};
+  _.each(places, function(place){
+    coordMap[place.page_id.toString()] = place.geometry.coordinates;
+  });
+  _.each(photos, function(photo){
+    photo.coords = coordMap[photo.place_id];
+  });
+}
+
+function reconcileBackdates(photos) {
+  _.each(photos, function(photo) {
+    if (photo.backdated_time) 
+      photo.created = photo.backdated_time;
+  });
+}
+
+function standardize(photos) {
+  return _.map(photos, function(photo) {
+    return {
+      "created": photo.created,
+      "coordinates": photo.coords,
+      "description": photo.caption,
+      "src": photo.src
+    }
+  });
+}
+
 
 function buildUrl(access_token) {
   var query = JSON.stringify({
-    "photos": "select object_id, owner, caption, backdated_time, created, place_id  FROM photo WHERE (object_id IN (SELECT object_id FROM photo_tag WHERE subject='544095626') or owner = 544095626 or owner = 424460880913112) and created > 1339457709 and place_id <> '' order by created",
+    "photos": "select object_id, owner, caption, backdated_time, created, src_small, place_id  FROM photo WHERE (object_id IN (SELECT object_id FROM photo_tag WHERE subject='544095626') or owner = 544095626 or owner = 424460880913112) and created > 1339457709 and place_id <> '' order by created",
     "places": "select page_id, geometry, name, type from place where page_id in (select place_id from #photos)" 
   });
   
-  var url = 'https://graph.facebook.com/fql?q=' + escape(query) + '&access_token=' + 'access_token';
+  return 'https://graph.facebook.com/fql?q=' + escape(query) + '&access_token=' + access_token;
+}
+
+function extractResults(fb_json) {
+  return {
+    'photos': fb_json.data[0].fql_result_set,
+    'places': fb_json.data[1].fql_result_set
+  };
 }
